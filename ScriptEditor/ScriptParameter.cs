45a6dffe50460f81e4aa9c1bc0ac7538c3e9a4f2
Takayuki Ozaki <uyamae@gmail.com>
2017-06-17 00:38:25 +0900

    first commit

---
+++ ScriptEditor/ScriptParameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptEditor
+{
+    public class ScriptParameter
+    {
+        public ScriptParameter()
+        {
+
+        }
+        public string ParamName { get; set; }
+        public string Value { get; set; }
+    }
+}
