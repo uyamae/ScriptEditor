f405233798b6c7a231ee988d29454b661e2f3c36
Takayuki Ozaki <uyamae@gmail.com>
2017-06-17 09:33:25 +0900

    add ScriptCommand class and view

---
+++ ScriptEditor/ScriptCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptEditor
+{
+    public class ScriptCommand
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ScriptCommand()
+        {
+            Parameters = new List<ScriptParameter>();
+        }
+        /// <summary>
+        /// コマンド名
+        /// </summary>
+        public string CommandName { get; set; }
+        /// <summary>
+        /// パラメータリスト
+        /// </summary>
+        public List<ScriptParameter> Parameters { get; set; }
+    }
+}
