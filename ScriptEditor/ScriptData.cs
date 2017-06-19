b112a0a084b317f3caced22aba266987a22e9075
Takayuki Ozaki <uyamae@gmail.com>
2017-06-19 11:34:14 +0900

    add ScriptData class and view

---
+++ ScriptEditor/ScriptData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptEditor
+{
+    /// <summary>
+    /// 一連のスクリプト
+    /// </summary>
+    public class ScriptData
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ScriptData()
+        {
+            CommandList = new List<ScriptCommand>();
+        }
+        /// <summary>
+        /// スクリプトID
+        /// </summary>
+        public string ScriptId { get; set; }
+        /// <summary>
+        /// スクリプトの説明
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// コマンドリスト
+        /// </summary>
+        public List<ScriptCommand> CommandList { get; set; }
+    }
+}
